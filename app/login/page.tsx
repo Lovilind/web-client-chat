a1032dd64121a04ba9ad4b76f8a16ca930438a90
fe-sonday <mrsmiler1106@gmail.com>
2024-05-12 20:59:01 +0900

    eslint airbnb rule 적용과 prettier 설정

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,6 @@
 export default function LoginPage() {
+  const test = 123;
+  console.log('eslint 테스트', test);
+
   return <div>로그인 페이지!!</div>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import Link from "next/link";
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href={"/login"}>
+      <Link href={'/login'}>
         <button>로그인하기</button>
       </Link>
     </main>
