43019ad7b7256f6dbe737585d6277fdfaf5e48c0
lemony452 <gij9808@gmail.com>
2024-05-22 19:55:22 +0900

    refactor: ChatRoom 컴포넌트로 병합

--- app/chat/[roomId]/page.tsx
+++ app/chat/[roomId]/page.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import InputBox from './(components)/InputBox';
-import MessageList from './(components)/MessageList';
+import ChatRoom from './(components)/ChatRoom';
 
 export default function RoomId({ params }: { params: { roomId: string } }) {
   return (
     <section className="p-3 w-full max-w-screen-md	h-full bg-blue-200">
-      <div className="relative w-full h-full flex justify-center">
-        <MessageList roomId={params.roomId} />
-        <InputBox />
-      </div>
+      <ChatRoom roomId={params.roomId} />
     </section>
   );
 }
