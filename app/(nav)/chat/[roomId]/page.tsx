ceef119bdab93775ef0a70e56ae8704f6ea8c426
KimJeongKyu-at-015982877895 <naron@torooc.com>
2024-05-22 15:04:05 +0900

    rename: 폴더 구조 옮기기

--- app/(nav)/chat/[roomId]/page.tsx
+++ app/(nav)/chat/[roomId]/page.tsx
@@ -4,8 +4,8 @@ import MessageList from './(components)/MessageList';
 
 export default function RoomId({ params }: { params: { roomId: string } }) {
   return (
-    <section className="p-3 w-full max-w-screen-md	h-full bg-blue-200">
-      <div className="relative w-full h-full flex justify-center">
+    <section className="h-full w-full max-w-screen-md	bg-blue-200 p-3">
+      <div className="relative flex h-full w-full justify-center">
         <MessageList roomId={params.roomId} />
         <InputBox />
       </div>
