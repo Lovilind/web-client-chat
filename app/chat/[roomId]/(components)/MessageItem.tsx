5e593ae38f13997717493bc54b45812d957a7301
lemony452 <gij9808@gmail.com>
2024-05-22 19:52:30 +0900

    refactor: MessageBox -> MessageItem 으로 컴포넌트명 수정

--- app/chat/[roomId]/(components)/MessageItem.tsx
+++ app/chat/[roomId]/(components)/MessageItem.tsx
@@ -2,11 +2,11 @@ import { UserThumbnail } from '@/components/UserThumbnail';
 import { ReceiveData } from '@/models/StompData';
 import React from 'react';
 
-interface MessageBoxProps {
+interface MessageItemProps {
   messageData: ReceiveData;
 }
 
-export default function MessageBox({ messageData }: MessageBoxProps) {
+export default function MessageItem({ messageData }: MessageItemProps) {
   return (
     <div className="flex flex-col gap-2">
       <div className="flex items-center gap-1">
