c0228aac5e13c6755e545bff754364f07022c6b8
lemony452 <gij9808@gmail.com>
2024-05-29 09:10:00 +0900

    feat: 테스트용 채팅 히스토리 데이터 fetch 추가

--- app/(nav)/chat/[roomId]/(components)/ChatRoom.tsx
+++ app/(nav)/chat/[roomId]/(components)/ChatRoom.tsx
@@ -6,20 +6,36 @@ import MessageList from './MessageList';
 import MessageInput from './MessageInput';
 import { Client } from '@stomp/stompjs';
 import { StompData } from '@/models/StompData';
+import { BaseAPI } from '@/utils/apis';
+import { ChatHistoryData } from '@/models/ChatHistoryData';
 
 type ChatRoomProps = Pick<StompData, 'roomId'>;
 
 export default function ChatRoom({ roomId }: ChatRoomProps) {
   const userInputRef = useRef<HTMLInputElement>(null);
   const [stompClient, setStompClient] = useState<Client>();
+  const [chatHistory, setChatHistory] = useState<ChatHistoryData>();
 
   // 로그인 구현 전 임시로 유저 설정
   // 1, 2, 3 3가지로 테스트할것
-  const handleUser = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleUser = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+
     if (userInputRef.current?.value) {
       const newStompClient = stomps.newStompClient(userInputRef.current.value);
       setStompClient(newStompClient);
+
+      try {
+        const chatHistoryData: ChatHistoryData = await BaseAPI({
+          method: 'GET',
+          url: '/messages',
+          headers: { Authorization: userInputRef.current.value },
+          params: { roomId },
+        });
+        setChatHistory(chatHistoryData);
+      } catch {
+        alert('채팅 내역 가져오는데 실패');
+      }
     }
   };
 
@@ -31,16 +47,20 @@ export default function ChatRoom({ roomId }: ChatRoomProps) {
   };
 
   return (
-    <div className="relative w-full h-full flex justify-center">
+    <div className="relative flex h-full w-full justify-center">
       {stompClient ? (
         <>
           <button
             onClick={handleLeaveChatRoom}
-            className="absolute right-1 top-1 bg-yellow-300 rounded-md px-2"
+            className="absolute right-1 top-1 rounded-md bg-yellow-300 px-2"
           >
             나가기
           </button>
-          <MessageList stompClient={stompClient} roomId={roomId}></MessageList>
+          <MessageList
+            chatHistory={chatHistory}
+            stompClient={stompClient}
+            roomId={roomId}
+          ></MessageList>
           <MessageInput
             stompClient={stompClient}
             roomId={roomId}
@@ -54,7 +74,7 @@ export default function ChatRoom({ roomId }: ChatRoomProps) {
             type="text"
             placeholder="임시 유저 토큰"
           />
-          <button className="bg-blue-500 rounded-md px-2" type="submit">
+          <button className="rounded-md bg-blue-500 px-2" type="submit">
             확인
           </button>
         </form>
