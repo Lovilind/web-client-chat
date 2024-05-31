51ef72e00201f19c0cf7f1e6b466bbab8ceb2d02
jg <jgkang9402@gmail.com>
2024-05-31 18:06:37 +0900

    fix: mswprovider수정

--- app/MswProvider.tsx
+++ app/MswProvider.tsx
@@ -1,31 +1,25 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export function MswProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [mockingEnabled, enableMocking] = useState(false);
-
   useEffect(() => {
     async function enableApiMocking() {
-      /**
-       * @fixme Next puts this import to the top of
-       * this module and runs it during the build
-       * in Node.js. This makes "msw/browser" import to fail.
-       */
-      const { worker } = await import('../mocks/browser');
-      await worker.start();
-      enableMocking(true);
+      if (typeof window === 'undefined') {
+        const { server: serverWorker } = await import('../mocks/server');
+        console.log(serverWorker);
+        serverWorker.listen();
+      } else {
+        const { worker } = await import('../mocks/browser');
+        await worker.start();
+      }
     }
 
     enableApiMocking();
   }, []);
-
-  if (!mockingEnabled) {
-    return null;
-  }
 
   return <>{children}</>;
 }
