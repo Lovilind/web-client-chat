571b959b431c7317452f58f70e05ead10b28c5c2
jg <jgkang9402@gmail.com>
2024-06-04 16:03:28 +0900

    style: header z-index수정

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import Nav from './Nav';
 
 const Header = () => {
   return (
-    <header className="z-1 sticky top-0 w-full overflow-hidden border-b bg-white px-8 py-4 lg:relative lg:flex lg:w-[130px] lg:items-center lg:justify-center lg:border-b-0 lg:border-r lg:p-0">
+    <header className="sticky top-0 z-2 w-full overflow-hidden border-b bg-white px-8 py-4 lg:relative lg:flex lg:w-[130px] lg:items-center lg:justify-center lg:border-b-0 lg:border-r lg:p-0">
       <div className="lg:fixed lg:top-5 lg:h-[calc(100vh-50px)]">
         <div className="flex h-[80%] items-center justify-between lg:flex-col">
           <div className="w-6 lg:hidden"></div>
