3d3439067b907514ed3a3a8b8acce4f280560335
jg <jgkang9402@gmail.com>
2024-07-16 23:17:02 +0900

    feat: 채팅 생성,제거 api

--- controller/chatController.ts
+++ controller/chatController.ts
@@ -1,8 +1,25 @@
 import axiosInstance from '@/utils/axiosInstance';
 
 export const getAllChatList = async () => {
-  return axiosInstance.get('/chatroom?page=1&limit=1');
+  return axiosInstance.get('/chatroom?page=1&limit=10');
 };
 export const getMyChatList = async () => {
-  return axiosInstance.get('/chatroom/my?page=1&limit=1');
+  return axiosInstance.get('/chatroom/my?page=1&limit=10');
+};
+
+export const postCreateChatRoom = async ({
+  chatRoomId,
+  userId,
+}: {
+  chatRoomId: string;
+  userId: string;
+}) => {
+  axiosInstance.post(`/chatroom/${chatRoomId}`, { chatRoomId, userId });
+};
+export const deleteChatRoom = async ({
+  chatRoomId,
+}: {
+  chatRoomId: string;
+}) => {
+  axiosInstance.delete(`/chatroom/${chatRoomId}`);
 };
